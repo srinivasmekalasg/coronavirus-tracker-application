4bbbd34492334462024fe4392a499f2d1e92dfd0
srinivasmekalasg <60573614+srinivasmekalasg@users.noreply.github.com>
2020-03-19 20:37:49 +0800

    coronavirus-tracker-application
    coronavirus-tracker-application using Spring boot + Thymelease + Java 11.
    
    - Calling the Updated .csv file from Git on daily basis.
    
    https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv

---
+++ coronavirus-tatus-tracker-application1/src/main/java/com/main/srini/CoronavirusTatusTrackerApplication1Application.java
@@ -0,0 +1,13 @@
+package com.main.srini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTatusTrackerApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTatusTrackerApplication1Application.class, args);
+	}
+
+}
---
+++ coronavirus-tatus-tracker-application1/src/test/java/com/main/srini/CoronavirusTatusTrackerApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.srini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTatusTrackerApplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
